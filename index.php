28697f341329f0f108ef7dc40ef62a0691f1ca14
Muffin <MuffinzCat@outlook.com>
2022-10-07 18:14:46 +0200

    Implemented api for displaying customer info, and made created at and updated at fields disabled due to visual clutter.

--- index.php
+++ index.php
@@ -17,24 +17,56 @@
             <thead>
             <tr>
                 <th>
-                    Customer Table:
+                    ID
                 </th>
+                <td>Email</td>
+                <td>First name</td>
+                <td>Last name</td>
+                <td>Adress</td>
+                <td>Zip Code</td>
+                <td>City</td>
+                <td>Tel.</td>
+<!--                <td>Created At</td>-->
+<!--                <td>Updated At</td>-->
             </tr>
             </thead>
             <tbody>
             <?php
-            for ($x = 0; $x < 10; $x++) {
-                echo "<tr>
-<th scope='row'>id</th>
-<td>email@email.exmp</td>
-<td>Firstname</td>
-<td>Lastname</td>
-<td>Adress 3\n1234xa Mpllen</td>
-<td>1234ZP</td>
-<td>(TEL) 1234567890</td>
-<td>CREATED AT</td>
-<td>UPDATED AT</td>
-</tr>";
+
+            $url = 'https://assignment.sunergetic.nl/api/v1/customers';
+            $headers = array(
+                    'Accept: application/json',
+                    'Content-Type: application/x-www-form-urlencoded',
+                    'token: 6vTr2lFu1P8i6mf3NJuqK8CPg3PG5y'
+            );
+
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL,$url);
+            curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER,$headers);
+
+            $data = curl_exec($curl);
+            $data_obj=json_decode($data);
+
+            curl_close($curl);
+
+//            print_r($data);
+
+
+            foreach ($data_obj->data as $users) {
+                echo "
+                <tr>
+                    <th scope='row'>$users->id</th>
+                    <td>$users->email</td>
+                    <td>$users->firstname</td>
+                    <td>$users->lastname</td>
+                    <td>$users->address</td>
+                    <td>$users->zipcode</td>
+                    <td>$users->city</td>
+                    <td>$users->phone</td>
+<!--                    <td>$users->created_at</td>
+                    <td>$users->updated_at</td> -->
+                </tr>";
             }
             ?>
             </tbody>
